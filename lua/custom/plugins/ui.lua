f632be3dcf43866aded3695265dc61c77856fec1
Tima Gutmensch <tima_gutmensch@proton.me>
2026-09-07 16:39:08 +0800

    add maintained tabline

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,15 @@
+vim.pack.add { 'https://github.com/nvim-mini/mini.tabline' }
+require('mini.tabline').setup {
+  -- Whether to show file icons (requires 'mini.icons')
+  show_icons = true,
+
+  -- Function which formats the tab label
+  -- By default surrounds with space and possibly prepends with icon
+  -- format = nil,
+
+  -- Where to show tabpage section in case of multiple vim tabpages.
+  -- One of 'left', 'right', 'none'.
+  tabpage_section = 'left',
+}
+vim.keymap.set('n', '<S-h>', '<Cmd>bprevious<CR>', { desc = 'Prev Buffer' })
+vim.keymap.set('n', '<S-l>', '<Cmd>bnext<CR>', { desc = 'Next Buffer' })

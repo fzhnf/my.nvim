3b03a9370a9cf621efe2ea04b73947cde4bc956b
Tima Gutmensch <tima_gutmensch@proton.me>
2026-09-09 03:03:06 +0800

    use markdownlint-cli2

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -4,13 +4,13 @@ vim.pack.add { 'https://github.com/mfussenegger/nvim-lint' }
 
 local lint = require 'lint'
 lint.linters_by_ft = {
-  markdown = { 'markdownlint' }, -- Make sure to install `markdownlint` via mason / npm
+  markdown = { 'markdownlint-cli2' },
 }
 
 -- To allow other plugins to add linters to require('lint').linters_by_ft,
 -- instead set linters_by_ft like this:
--- lint.linters_by_ft = lint.linters_by_ft or {}
--- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+lint.linters_by_ft = lint.linters_by_ft or {}
+lint.linters_by_ft['markdown'] = { 'markdownlint-cli2' }
 --
 -- However, note that this will enable a set of default linters,
 -- which will cause errors unless these tools are available:
@@ -38,6 +38,8 @@ lint.linters_by_ft = {
 -- lint.linters_by_ft['ruby'] = nil
 -- lint.linters_by_ft['terraform'] = nil
 -- lint.linters_by_ft['text'] = nil
+
+lint.linters['markdownlint-cli2'].args = { '--config', vim.fn.stdpath 'config' .. '/.markdownlint-cli2.yaml', '--' }
 
 -- Create autocommand which carries out the actual linting
 -- on the specified events.

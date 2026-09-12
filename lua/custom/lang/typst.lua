4f55cf34f4fe87effc3ad0b09f5d8f837078a004
Tima Gutmensch <tima_gutmensch@proton.me>
2026-09-12 14:51:11 +0800

    use typstyle formatter, and add tinymist pin to determine the main file from codebase with many typ files

--- lua/custom/lang/typst.lua
+++ lua/custom/lang/typst.lua
@@ -3,18 +3,13 @@ local lang = require 'custom.util.lang'
 
 lang.add_parser 'typst'
 lang.add_tool 'tinymist'
+vim.lsp.config('tinymist', { settings = { formatterMode = 'typstyle' } })
 vim.lsp.enable 'tinymist'
 
 vim.pack.add({
   {
     src = vp.gh 'chomosuke/typst-preview.nvim',
-    data = {
-      opts = {
-        dependencies_bin = {
-          tinymist = 'tinymist',
-        },
-      },
-    },
+    data = { opts = { dependencies_bin = { tinymist = 'tinymist' } } },
   },
 }, {
   load = function(plug_data)
@@ -32,9 +27,7 @@ vim.pack.add({
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'typst',
   callback = function(args)
-    vim.keymap.set('n', '<leader>cp', '<cmd>TypstPreviewToggle<cr>', {
-      buffer = args.buf,
-      desc = 'Toggle Typst Preview',
-    })
+    vim.keymap.set('n', '<leader>cp', '<cmd>TypstPreviewToggle<cr>', { buffer = args.buf, desc = 'Toggle Typst Preview' })
+    vim.keymap.set('n', '<leader>cP', '<cmd>LspTinymistPinMain<cr>', { buffer = args.buf, desc = 'Tinymist: Pin main file' })
   end,
 })

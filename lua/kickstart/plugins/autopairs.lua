a1efc644acbd35cefd50e1f711219275499d020e
Tima Gutmensch <tima_gutmensch@proton.me>
2026-09-18 23:59:01 +0800

    move dap to plugins, and remove all legacy kickstart codes

--- lua/kickstart/plugins/autopairs.lua
+++
@@ -1,7 +0,0 @@
-local vp = require 'custom.util.vimpack_helper'
-
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
-vim.pack.add { vp.gh 'windwp/nvim-autopairs' }
-require('nvim-autopairs').setup {}

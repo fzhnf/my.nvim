e8a1229272866d41ab42de64437536a034a931ae
Tima Gutmensch <tima_gutmensch@proton.me>
2026-09-19 10:32:08 +0800

    refactor to utilize selective_load

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -1,10 +1,25 @@
 local vp = require 'custom.util.vimpack_helper'
 
-vim.schedule(function()
-  vim.pack.add { vp.gh 'zbirenbaum/copilot.lua' }
-  require('copilot').setup {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-  }
-  vim.pack.add { vp.gh 'giuxtaposition/blink-cmp-copilot' }
-end)
+vim.pack.add({
+  src = vp.gh 'zbirenbaum/copilot.lua',
+  data = { opts = { suggestion = { enabled = false }, panel = { enabled = false } } },
+}, {
+  load = function(plug_data)
+    vim.api.nvim_create_autocmd('InsertEnter', {
+      once = true,
+      callback = function()
+        vim.cmd.packadd(plug_data.spec.name)
+        require('copilot').setup(plug_data.spec.data.opts)
+      end,
+    })
+  end,
+})
+
+vim.pack.add({ vp.gh 'giuxtaposition/blink-cmp-copilot' }, {
+  load = function(plug_data)
+    vim.api.nvim_create_autocmd('InsertEnter', {
+      once = true,
+      callback = function() vim.cmd.packadd(plug_data.spec.name) end,
+    })
+  end,
+})

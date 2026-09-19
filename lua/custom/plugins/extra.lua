44b97d5c92cc23cb9aeebdf6f28df1781e3b0210
Tima Gutmensch <tima_gutmensch@proton.me>
2026-09-19 10:35:04 +0800

    map the load event of cord.nvim

--- lua/custom/plugins/extra.lua
+++ lua/custom/plugins/extra.lua
@@ -1,12 +1,14 @@
 local vp = require 'custom.util.vimpack_helper'
 
-vim.pack.add { vp.gh 'vyfor/cord.nvim' }
-require('cord').setup {
-  display = { theme = 'minecraft', flavor = 'accent' },
-  buttons = {
-    {
-      label = 'View Repository',
-      url = function(opts) return opts.repo_url end,
+vim.schedule(function()
+  vim.pack.add { vp.gh 'vyfor/cord.nvim' }
+  require('cord').setup {
+    display = { theme = 'minecraft', flavor = 'accent' },
+    buttons = {
+      {
+        label = 'View Repository',
+        url = function(opts) return opts.repo_url end,
+      },
     },
-  },
-}
+  }
+end)
